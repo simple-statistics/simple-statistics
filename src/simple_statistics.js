67fbd41238480431279a87788f0f0a79ec84af2c
Tom MacWright <tom@macwright.org>
2012-06-24 13:17:32 -0400

    Don't make r_squared more complicated than it should be

--- src/simple_statistics.js
+++ src/simple_statistics.js
@@ -68,63 +68,38 @@
         return linreg;
     };
 
-    ss.r_squared = function() {
+    ss.r_squared = function(data, f) {
+        if (data.length < 2) return 1;
 
-        var r = {},
-            data,
-            f;
+        // Compute the average y value for the actual
+        // data set in order to compute the
+        // _total sum of squares_
+        var sum = 0, average;
+        for (var i = 0; i < data.length; i++) {
+            sum += data[i][1];
+        }
+        average = sum / data.length;
 
-        // Assign the data to the model.
-        r.data = function(x) {
-            if (!arguments.length) return data;
-            data = x.slice();
-            return r;
-        };
+        // Compute the total sum of squares - the
+        // squared difference between each point
+        // and the average of all points.
+        var sum_of_squares = 0;
+        for (var j = 0; j < data.length; j++) {
+            sum_of_squares += Math.pow(average - data[j][1], 2);
+        }
 
-        // Assign the estimator function, that is used
-        // to calculate the error in `r_squared` below.
-        r.f = function(x) {
-            if (!arguments.length) return f;
-            f = x;
-            return r;
-        };
+        // Finally estimate the error: the squared
+        // difference between the estimate and the actual data
+        // value at each point.
+        var err = 0;
+        for (var k = 0; k < data.length; k++) {
+            err += Math.pow(data[k][1] - f(data[k][0]), 2);
+        }
 
-        r.r_squared = function() {
-            if (data.length < 2) return 1;
-
-            // Compute the average y value for the actual
-            // data set in order to compute the
-            // _total sum of squares_
-            var sum = 0, average;
-            for (var i = 0; i < data.length; i++) {
-                sum += data[i][1];
-            }
-            average = sum / data.length;
-
-            // Compute the total sum of squares - the
-            // squared difference between each point
-            // and the average of all points.
-            var sum_of_squares = 0;
-            for (var j = 0; j < data.length; j++) {
-                sum_of_squares += Math.pow(average - data[j][1], 2);
-            }
-
-            // Finally estimate the error: the squared
-            // difference between the estimate and the actual data
-            // value at each point.
-            var err = 0;
-            for (var k = 0; k < data.length; k++) {
-                err += Math.pow(data[k][1] - f(data[k][0]), 2);
-            }
-
-            // As the error grows larger, it's ratio to the
-            // sum of squares increases and the r squared
-            // value grows lower.
-            return 1 - (err / sum_of_squares);
-
-        };
-
-        return r;
+        // As the error grows larger, it's ratio to the
+        // sum of squares increases and the r squared
+        // value grows lower.
+        return 1 - (err / sum_of_squares);
     };
 
 
--- test/r_squared.test.js
+++ test/r_squared.test.js
@@ -4,11 +4,11 @@ var ss = require('../');
 it('says that the r squared of a two-point line is perfect', function() {
     var d = [[0, 0], [1, 1]];
     var l = ss.linear_regression().data(d);
-    assert.equal(ss.r_squared().data(d).f(l.line()).r_squared(), 1);
+    assert.equal(ss.r_squared(d, l.line()), 1);
 });
 
 it('says that the r squared of a three-point line is not perfect', function() {
     var d = [[0, 0], [0.5, 0.2], [1, 1]];
     var l = ss.linear_regression().data(d);
-    assert.notEqual(ss.r_squared().data(d).f(l.line()).r_squared(), 1);
+    assert.notEqual(ss.r_squared(d, l.line()), 1);
 });
