685c4c830e4d84e0e700537f66aa6f091fc05ea7
Tom MacWright <tom@macwright.org>
2012-07-02 17:49:48 -0400

    Update comment, mention Matt in readme

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ as well as in node.js.
 
 # [Documentation](https://github.com/tmcw/simple-statistics/wiki)
 # [Tests](http://travis-ci.org/#!/tmcw/simple-statistics)
+
+# Contributors
+
+* Tom MacWright
+* [Matt Sacks](https://github.com/mattsa)
--- src/simple_statistics.js
+++ src/simple_statistics.js
@@ -274,8 +274,16 @@
     };
 
     // # [t-test](http://en.wikipedia.org/wiki/Student's_t-test)
+    //
     // This is to compute a one-sample t-test, comparing the mean
     // of a sample to a known value, x.
+    //
+    // in this case, we're trying to determine whether the
+    // population mean is equal to the value that we know, which is `x`
+    // here. usually the results here are used to look up a
+    // [p-value](http://en.wikipedia.org/wiki/P-value), which, for
+    // a certain level of significance, will let you determine that the
+    // null hypothesis can or cannot be rejected.
     ss.t_test = function(sample, x) {
       // The mean of the sample
       var sample_mean = ss.mean(sample);
