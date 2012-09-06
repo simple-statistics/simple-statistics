2bc171e461e767847c5154b47484f6ceded04e77
jthomm <jthomm@yahoo.com>
2012-09-06 19:34:47 -0300

    Ensure `.median` sorts numerically
    If `.median` sorts lexicographically then `ss.median([8, 9, 10])` will be 8.

--- test/median.test.js
+++ test/median.test.js
@@ -13,11 +13,15 @@ it('can get the median of four numbers', function() {
     assert.equal(ss.median([1, 2, 3, 4]), 2.5);
 });
 
-it('gives null for the median of an empty lsit', function() {
+it('gives null for the median of an empty list', function() {
     assert.equal(ss.median([]), null);
 });
 
-it('does not chang the sorting order of its input', function() {
+it('sorts numbers numerically', function() {
+    assert.equal(ss.median([8, 9, 10]), 9);
+});
+
+it('does not change the sorting order of its input', function() {
     var x = [1, 0];
     assert.equal(ss.median(x), 0.5);
     assert.equal(x[0], 1);
