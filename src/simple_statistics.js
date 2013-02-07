625caa51b014977431c114c6dc8021fcfae0a0af
Tom MacWright <tom@macwright.org>
2013-02-07 15:22:25 -0500

    Improve covariance documentation. Fixes #21

--- src/simple_statistics.js
+++ src/simple_statistics.js
@@ -355,22 +355,33 @@
 
     // # [covariance](http://en.wikipedia.org/wiki/Covariance)
     //
-    // sample covariance of two datasets: how much do the two datasets move together?
-    ss.sample_covariance = function(x, y){
+    // sample covariance of two datasets:
+    // how much do the two datasets move together?
+    // x and y are two datasets, represented as arrays of numbers.
+    ss.sample_covariance = function(x, y) {
+
         // The two datasets must have the same length which must be more than 1
         if (x.length <= 1 || x.length != y.length){
           return null;
         }
 
-        var xmean = ss.mean(x);
-        var ymean = ss.mean(y);
+        // determine the mean of each dataset so that we can judge each
+        // value of the dataset fairly as the difference from the mean. this
+        // way, if one dataset is [1, 2, 3] and [2, 3, 4], their covariance
+        // does not suffer because of the difference in absolute values
+        var xmean = ss.mean(x),
+            ymean = ss.mean(y),
+            sum = 0;
 
-        var sum = 0.0;
-
+        // for each pair of values, the covariance increases when their
+        // difference from the mean is associated - if both are well above
+        // or if both are well below
+        // the mean, the covariance increases significantly.
         for (var i = 0; i < x.length; i++){
-          sum += (x[i] - xmean) * (y[i] - ymean);
+            sum += (x[i] - xmean) * (y[i] - ymean);
         }
 
+        // the covariance is weighted by the length of the datasets.
         return sum / (x.length - 1);
     };
 
