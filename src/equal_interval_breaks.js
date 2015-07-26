febda3dbe353f30e25f5affee9c91b4c39419aa3
Tom MacWright <tom@macwright.org>
2015-07-26 13:14:47 -0400

    Implement equalIntervalBreaks. Fixes #57
    cc @morganherlocker could you do a quick code review?

---
+++ src/equal_interval_breaks.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var max = require('./max'),
+  min = require('./min');
+
+/**
+ * Given an array of data, this will find the extent of the
+ * data and return an array of breaks that can be used
+ * to categorize the data into a number of classes. The
+ * returned array will always be 1 longer than the number of
+ * classes because it includes the minimum value.
+ *
+ * @param {Array<number>} data input data, as an array of number values
+ * @param {number} nClasses number of desired classes
+ * @returns {Array<number>} array of class break positions
+ * @example
+ * equalIntervalBreaks([1, 2, 3, 4, 5, 6], 4); //= [1, 2.25, 3.5, 4.75, 6]
+ */
+function equalIntervalBreaks(data, nClasses) {
+
+  // the first break will always be the minimum value
+  // in the dataset
+  var breaks = [min(data)];
+
+  // The size of each break is the full range of the data
+  // divided by the number of classes requested
+  var breakSize = (max(data) - min(data)) / nClasses;
+
+  // In the case of nClasses = 1, this loop won't run
+  // and the returned breaks will be [min, max]
+  for (var i = 1; i < nClasses; i++) {
+    breaks.push(breaks[0] + breakSize * i);
+  }
+
+  // the last break will always be the
+  // maximum.
+  breaks.push(max(data));
+
+  return breaks;
+}
+
+module.exports = equalIntervalBreaks;
---
+++ test/equal_interval_breaks.test.js
@@ -0,0 +1,12 @@
+/* eslint no-shadow: 0 */
+'use strict';
+
+var test = require('tape');
+var equalIntervalBreaks = require('../src/equal_interval_breaks');
+
+test('equalIntervalBreaks', function(t) {
+    t.deepEqual(equalIntervalBreaks([1, 2, 3, 4, 5, 6], 4), [ 1, 2.25, 3.5, 4.75, 6 ], 'three breaks');
+    t.deepEqual(equalIntervalBreaks([1, 2, 3, 4, 5, 6], 2), [1, 3.5, 6], 'two breaks');
+    t.deepEqual(equalIntervalBreaks([1, 2, 3, 4, 5, 6], 1), [1, 6], 'one break');
+    t.end();
+});
