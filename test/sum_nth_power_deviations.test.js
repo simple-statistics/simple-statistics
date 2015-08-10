fe01a7e52e743d5cba89ad1db59299c5fae6b95d
Tom MacWright <tom@macwright.org>
2015-08-09 23:49:11 -0400

    Remove unused include

--- test/sum_nth_power_deviations.test.js
+++ test/sum_nth_power_deviations.test.js
@@ -4,10 +4,6 @@
 var test = require('tape');
 var ss = require('../');
 
-function rnd(x) {
-    return Math.round(x * 1000) / 1000;
-}
-
 test('sumNthPowerDeviations', function(t) {
     t.equal(
         ss.sumNthPowerDeviations([0, 0, 0], 2),
