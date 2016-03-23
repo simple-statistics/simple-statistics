60a9da5786e0dc9229ed8b28700c1a1078592203
Tom MacWright <tom@macwright.org>
2016-03-23 08:42:20 -0400

    Kahan sum by default

---
+++ src/sum_simple.js
@@ -0,0 +1,23 @@
+'use strict';
+/* @flow */
+
+/**
+ * The simple [sum](https://en.wikipedia.org/wiki/Summation) of an array
+ * is the result of adding all numbers together, starting from zero.
+ *
+ * This runs on `O(n)`, linear time in respect to the array
+ *
+ * @param {Array<number>} x input
+ * @return {number} sum of all input numbers
+ * @example
+ * console.log(sum([1, 2, 3])); // 6
+ */
+function sumSimple(x/*: Array<number> */)/*: number */ {
+    var value = 0;
+    for (var i = 0; i < x.length; i++) {
+        value += x[i];
+    }
+    return value;
+}
+
+module.exports = sumSimple;
--- test/sum.test.js
+++ test/sum.test.js
@@ -17,20 +17,20 @@ test('sum', function(t) {
     t.end();
 });
 
-test('sumKahan', function(t) {
+test('sumSimple', function(t) {
     t.test('can get the sum of two numbers', function(t) {
-        t.equal(ss.sumKahan([1, 2]), 3);
+        t.equal(ss.sumSimple([1, 2]), 3);
         t.end();
     });
 
     t.test('can get the sum of two numbers', function(t) {
-        t.equal(ss.sum([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7]), 15.299999999999999);
-        t.equal(ss.sumKahan([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7]), 15.3);
+        t.equal(ss.sumSimple([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7]), 15.299999999999999);
+        t.equal(ss.sum([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7]), 15.3);
         t.end();
     });
 
     t.test('the sum of no numbers is zero', function(t) {
-        t.equal(ss.sumKahan([]), 0);
+        t.equal(ss.sumSimple([]), 0);
         t.end();
     });
     t.end();
