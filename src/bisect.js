ad76b0d4d8955b0862de7702d4ed47d7dcd08f63
Jamie Neubert Pedersen <zajaka@hotmail.com>
2016-07-13 12:55:23 +0200

    Add type annotation

--- src/bisect.js
+++ src/bisect.js
@@ -17,7 +17,7 @@
  * @example
  * bisect(Math.cos,0,4,100,0.003)
  */
-function bisect(f, a, b, nMax, tol) {
+function bisect(f/*: (x: any) => number */, a/*: number */, b/*: number */, nMax/*: number */, tol/*: number */) {
     if (typeof f !== 'function') throw new SyntaxError('f must be a function');
     
     var n = 1;
