44dcff3c428586592a76ce6c08e889039678354b
Jamie Neubert Pedersen <zajaka@hotmail.com>
2016-07-13 12:57:49 +0200

    Add return of function in example

--- src/bisect.js
+++ src/bisect.js
@@ -15,7 +15,7 @@
  * @returns {Number} estimated root value
  * 
  * @example
- * bisect(Math.cos,0,4,100,0.003)
+ * bisect(Math.cos,0,4,100,0.003); // => 1.572265625
  */
 function bisect(f/*: (x: any) => number */, a/*: number */, b/*: number */, nMax/*: number */, tol/*: number */) {
     if (typeof f !== 'function') throw new SyntaxError('f must be a function');
