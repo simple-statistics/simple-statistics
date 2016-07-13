08f6bfdd08d840c9c960a23a6430b610c6dd1ec2
Tom MacWright <tom@macwright.org>
2016-07-13 15:50:55 -0400

    Cover bisect iteration error line

--- test/bisect.test.js
+++ test/bisect.test.js
@@ -9,6 +9,9 @@ test('bisect', function(t) {
         t.equal(Number(bisect(Math.sin, 1, 4, 100, 0.003).toFixed(4)), 3.1416);
         t.equal(Number(bisect(Math.cos, 0, 4, 100, 0.003).toFixed(4)), 1.5723);
         t.throws(function() {
+            t.equal(Number(bisect(Math.cos, 0, 4, 1, 0.003).toFixed(4)), 1.5723);
+        }, 'Throws if it exceeds the number of iterations allowed');
+        t.throws(function() {
             bisect(0);
         }, 'Throws with syntax error f must be a function');
         t.end();
