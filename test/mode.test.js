fced9035a577244fef9b437511e9ccfdd6e6a8be
Tom MacWright <tom@macwright.org>
2017-02-20 17:42:06 -0500

    feat(mode): Implement modeFast, an indexed mode implementation
    This differs from the existing mode implementation, ss.mode, because
    
    * It is compatible with non-number types
    * It's faster
    * It requires Map support in your JavaScript environment

--- test/mode.test.js
+++ test/mode.test.js
@@ -5,36 +5,46 @@ var test = require('tap').test;
 var ss = require('../');
 
 test('mode', function(t) {
-    t.test('the mode of a single-number array is that one number', function(t) {
-        t.equal(ss.mode([1]), 1);
-        t.end();
-    });
 
-    t.test('the mode of a two-number array is that one number', function(t) {
-        t.equal(ss.mode([1, 1]), 1);
-        t.end();
-    });
+    ['mode', 'modeFast'].forEach(function (modeName) {
 
-    t.test('other cases', function(t) {
-        t.equal(ss.mode([1, 1, 2]), 1);
-        t.equal(ss.mode([1, 1, 2, 3]), 1);
-        t.equal(ss.mode([1, 1, 2, 3, 3]), 1);
-        t.equal(ss.mode([1, 1, 2, 3, 3, 3]), 3);
-        t.equal(ss.mode([1, 1, 2, 2, 2, 2, 3, 3, 3]), 2);
-        t.equal(ss.mode([1, 2, 3, 4, 5]), 1);
-        t.equal(ss.mode([1, 2, 3, 4, 5, 5]), 5);
-        t.equal(ss.mode([1, 1, 1, 2, 2, 3, 3, 4, 4]), 1);
-        t.end();
-    });
+        t.test(modeName, function (t) {
+            var modeFn = ss[modeName];
 
-    t.test('the mode of an empty array is null', function(t) {
-        t.ok(isNaN(ss.mode([])));
-        t.end();
-    });
+            t.test('the mode of a single-number array is that one number', function(t) {
+                t.equal(modeFn([1]), 1);
+                t.end();
+            });
 
-    t.test('the mode of a three-number array with two same numbers is the repeated one', function(t) {
-        t.equal(ss.mode([1, 2, 2]), 2);
-        t.end();
+            t.test('the mode of a two-number array is that one number', function(t) {
+                t.equal(modeFn([1, 1]), 1);
+                t.end();
+            });
+
+            t.test('other cases', function(t) {
+                t.equal(modeFn([1, 1, 2]), 1);
+                t.equal(modeFn([1, 1, 2, 3]), 1);
+                t.equal(modeFn([1, 1, 2, 3, 3]), 1);
+                t.equal(modeFn([1, 1, 2, 3, 3, 3]), 3);
+                t.equal(modeFn([1, 2, 2, 2, 1, 2, 3, 3, 3]), 2);
+                t.equal(modeFn([1, 2, 3, 4, 5]), 1);
+                t.equal(modeFn([1, 2, 3, 4, 5, 5]), 5);
+                t.equal(modeFn([1, 2, 2, 3, 3, 4, 1, 4, 1]), 1);
+                t.end();
+            });
+
+            t.test('the mode of an empty array is null', function(t) {
+                t.ok(isNaN(modeFn([])));
+                t.end();
+            });
+
+            t.test('the mode of a three-number array with two same numbers is the repeated one', function(t) {
+                t.equal(modeFn([1, 2, 2]), 2);
+                t.end();
+            });
+
+            t.end();
+        });
     });
 
     t.test('mode sorted', function(t) {
