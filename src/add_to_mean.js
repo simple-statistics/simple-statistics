515d71c34591bb425189fdc4accb7f2e407a714d
Tom MacWright <tmcw@users.noreply.github.com>
2017-02-20 10:34:34 -0500

    Fix documentation, touch to rebuild

--- src/add_to_mean.js
+++ src/add_to_mean.js
@@ -8,16 +8,16 @@
  * the number of elements in the list that produced it and the new
  * value to add.
  *
- * @param {Number} current mean
- * @param {Number} number of items in the list
- * @param {Number} the added value
+ * @param {Number} mean current mean
+ * @param {Number} n number of items in the list
+ * @param {Number} newValue the added value
  * @returns {Number} the new mean
  *
  * @example
  * addToMean(14, 5, 53); // => 20.5
  */
-function addToMean(mean /*: number*/, n/*: number */, x/*: number */)/*: number */ {
-    return mean + ((x - mean) / (n + 1));
+function addToMean(mean /*: number*/, n/*: number */, newValue/*: number */)/*: number */ {
+    return mean + ((newValue - mean) / (newValue + 1));
 }
 
 module.exports = addToMean;
