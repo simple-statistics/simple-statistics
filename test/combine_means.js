1079a2d16b3e6101c640b3f121c1f814d0eccc54
Yomguithereal <guillaumeplique@gmail.com>
2017-02-27 17:25:57 +0100

    Fixing unit test formulation

--- test/combine_means.js
+++ test/combine_means.js
@@ -5,7 +5,7 @@ var test = require('tap').test;
 var ss = require('../');
 
 test('combineMeans', function(t) {
-    t.test('can get add a single value to a mean', function(t) {
+    t.test('can combine the means of two lists', function(t) {
         var values1 = [8, 3, 4];
         var values2 = [2, 6, 4];
         t.equal(ss.combineMeans(ss.mean(values1), values1.length, ss.mean(values2), values2.length), 4.5);
