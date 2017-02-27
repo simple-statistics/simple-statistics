d2b9f8d5a052d50851ff08c20c35797646ef9993
Yomguithereal <guillaumeplique@gmail.com>
2017-02-27 17:24:27 +0100

    Adding combineVariances

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ ss.combinationsReplacement = require('./src/combinations_replacement');
 // measures of centrality
 ss.addToMean = require('./src/add_to_mean');
 ss.combineMeans = require('./src/combine_means');
+ss.combineVariances = require('./src/combine_variances');
 ss.geometricMean = require('./src/geometric_mean');
 ss.harmonicMean = require('./src/harmonic_mean');
 ss.mean = ss.average = require('./src/mean');
