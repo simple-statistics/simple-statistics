1b22058d9a2dab5cc11dc9b35d4e87373edd9649
Tom Macwright <tom@macwright.org>
2017-04-11 13:54:37 -0400

    perf(sampleSkewness): Improve sampleSkewness performance
    Avoids a recomputation of the mean value
    
    Refs #194

--- src/sample_skewness.js
+++ src/sample_skewness.js
@@ -1,8 +1,7 @@
 'use strict';
 /* @flow */
 
-var sumNthPowerDeviations = require('./sum_nth_power_deviations');
-var sampleStandardDeviation = require('./sample_standard_deviation');
+var mean = require('./mean');
 
 /**
  * [Skewness](http://en.wikipedia.org/wiki/Skewness) is
@@ -21,16 +20,32 @@ var sampleStandardDeviation = require('./sample_standard_deviation');
  * sampleSkewness([2, 4, 6, 3, 1]); // => 0.590128656384365
  */
 function sampleSkewness(x /*: Array<number> */)/*:number*/ {
-    // The skewness of less than three arguments is null
-    var theSampleStandardDeviation = sampleStandardDeviation(x);
 
     if (x.length < 3) {
         throw new Error('sampleSkewness requires at least three data points');
     }
 
+    var meanValue = mean(x);
+    var tempValue;
+    var sumSquaredDeviations = 0;
+    var sumCubedDeviations = 0;
+
+    for (var i = 0; i < x.length; i++) {
+        tempValue = x[i] - meanValue;
+        sumSquaredDeviations += tempValue * tempValue;
+        sumCubedDeviations += tempValue * tempValue * tempValue;
+    }
+
+    // this is Bessels' Correction: an adjustment made to sample statistics
+    // that allows for the reduced degree of freedom entailed in calculating
+    // values from samples rather than complete populations.
+    var besselsCorrection = x.length - 1;
+
+    // Find the mean value of that list
+    var theSampleStandardDeviation = Math.sqrt(sumSquaredDeviations / besselsCorrection);
+
     var n = x.length,
-        cubedS = Math.pow(theSampleStandardDeviation, 3),
-        sumCubedDeviations = sumNthPowerDeviations(x, 3);
+        cubedS = Math.pow(theSampleStandardDeviation, 3);
 
     return n * sumCubedDeviations / ((n - 1) * (n - 2) * cubedS);
 }
