0d7c893727e409dc62824c5ddca303f65016ae04
Tom Macwright <tom@macwright.org>
2017-04-25 16:30:03 -0400

    chore: Update branch of website

--- scripts/update_website.sh
+++ scripts/update_website.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone -b update-version https://github.com/simple-statistics/simple-statistics.github.com.git
+git clone https://github.com/simple-statistics/simple-statistics.github.com.git
 node simple-statistics.github.com/update_version.js ../package.json
 cd simple-statistics.github.com
 git add index.html
