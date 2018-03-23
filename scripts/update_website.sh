b03551564be7a6cf8623feb2d690a5677a3c58eb
Tom MacWright <tom@macwright.org>
2018-03-23 16:29:08 -0700

    build: Make website build script robust for if you already have a directory local.

--- scripts/update_website.sh
+++ scripts/update_website.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm -r simple-statistics.github.com
 git clone https://github.com/simple-statistics/simple-statistics.github.com.git
 node simple-statistics.github.com/update_version.js ../package.json
 cd simple-statistics.github.com
