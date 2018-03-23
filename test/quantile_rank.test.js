f9d4dd9500989179b1dc2f8947561bda1ad789eb
Yomguithereal <guillaumeplique@gmail.com>
2018-03-23 18:05:53 +0100

    feat: adding quantileRank
    Related to #119

---
+++ test/quantile_rank.test.js
@@ -0,0 +1,19 @@
+/* eslint no-shadow: 0 */
+
+
+var test = require('tap').test;
+var ss = require('../');
+
+test('quantileRank', function(t) {
+    // Data and results from
+    // [Scipy](https://github.com/scipy/scipy/blob/master/scipy/stats/stats.py)
+    t.test('can get proper quantile ranks', function(t) {
+        t.equal(ss.quantileRank([3, 2, 4, 1], 3), 0.75);
+        t.equal(ss.quantileRank([4, 3, 2, 1, 3], 3), 0.7);
+        t.equal(ss.quantileRank([2, 1, 3, 4], 6), 1);
+        t.equal(ss.quantileRank([4, 1, 2, 3], -3), 0);
+        t.equal(ss.quantileRank([5, 2, 1, 3, 3], 4), 0.8);
+        t.end();
+    });
+    t.end();
+});
