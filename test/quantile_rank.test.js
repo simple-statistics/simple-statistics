84248ea1e25b9cf826df6c513204c93b8ec918d7
Guillaume Plique <guillaumeplique@gmail.com>
2018-03-24 00:22:34 +0100

    feat: adding quantileRankSorted (#285)
    Adds `quantileRankSorted`, a faster method for determining quantile rank if you have sorted data.

---
+++ test/quantile_rank.test.js
@@ -0,0 +1,20 @@
+/* eslint no-shadow: 0 */
+
+
+var test = require('tap').test;
+var ss = require('../');
+
+test('quantileRank', function(t) {
+    // Data and results from
+    // [Scipy](https://github.com/scipy/scipy/blob/master/scipy/stats/stats.py)
+    t.test('can get proper quantile ranks', function(t) {
+        t.equal(ss.quantileRank([3, 2, 4, 1], 3), 0.75);
+        t.equal(ss.quantileRank([4, 3, 2, 1, 3], 3), 0.7);
+        t.equal(ss.quantileRank([2, 1, 3, 4], 6), 1);
+        t.equal(ss.quantileRank([4, 1, 2, 3], -3), 0);
+        t.equal(ss.quantileRank([5, 2, 1, 3, 3], 4), 0.8);
+        t.equal(ss.quantileRank([5, 567, 1, 11, 2, 22, 4, 45, 3, 34], 5), 0.5);
+        t.end();
+    });
+    t.end();
+});
