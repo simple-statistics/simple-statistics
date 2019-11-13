7ff90cf39573a4f299d1d8ced05ac62652124ef4
Tom MacWright <tom@macwright.org>
2019-11-12 16:45:04 -0800

    build: Make uncompressed dist file actually uncompressed
    Switches from microbundle to rollup
    
    Fixes #394

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,33 @@
-import { uglify } from 'rollup-plugin-uglify'
+import { terser } from "rollup-plugin-terser";
 
-const input = 'index.js'
-const sourcemap = true
+const input = "index.js";
+const sourcemap = true;
 
-export default [{
-    input,
-    output: {
-        file: 'dist/simple-statistics.mjs',
-        format: 'es',
-        sourcemap
-    }
-}, {
-    input,
-    output: {
-        file: 'dist/simple-statistics.js',
-        format: 'cjs',
-        sourcemap
-    }
-},
-{
-    input,
-    output: {
-        file: 'dist/simple-statistics.min.js',
-        format: 'umd',
-        name: 'ss',
-        sourcemap
+export default [
+    {
+        input,
+        output: {
+            file: "dist/simple-statistics.mjs",
+            format: "es",
+            sourcemap
+        }
     },
-    plugins: [uglify()]
-}]
+    {
+        input,
+        output: {
+            file: "dist/simple-statistics.js",
+            format: "cjs",
+            sourcemap
+        }
+    },
+    {
+        input,
+        output: {
+            file: "dist/simple-statistics.min.js",
+            format: "umd",
+            name: "ss",
+            sourcemap
+        },
+        plugins: [terser()]
+    }
+];
