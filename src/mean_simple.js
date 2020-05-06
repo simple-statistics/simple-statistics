8e82333bf4d2101c990aa542e34b710240d77f36
James Hay <pokinoice@live.com>
2020-05-06 13:35:18 +0800

    feat: meanSimple method that uses sumSimple for higher performance
    Adds new meanSimple (and aliased averageSimple) methods that use simple addition internally for higher performance.

---
+++ src/mean_simple.js
@@ -0,0 +1,32 @@
+import sumSimple from "./sum_simple";
+
+/**
+ * The mean, _also known as average_,
+ * is the sum of all values over the number of values.
+ * This is a [measure of central tendency](https://en.wikipedia.org/wiki/Central_tendency):
+ * a method of finding a typical or central value of a set of numbers.
+ *
+ * The simple mean uses the successive addition method internally
+ * to calculate it's result. Errors in floating-point addition are
+ * not accounted for, so if precision is required, the standard {@link mean}
+ * method should be used instead.
+ *
+ * This runs on `O(n)`, linear time in respect to the array.
+ *
+ *
+ * @param {Array<number>} x sample of one or more data points
+ * @throws {Error} if the the length of x is less than one
+ * @returns {number} mean
+ * @example
+ * mean([0, 10]); // => 5
+ */
+function meanSimple(x) {
+    // The mean of no numbers is null
+    if (x.length === 0) {
+        throw new Error("meanSimple requires at least one data point");
+    }
+
+    return sumSimple(x) / x.length;
+}
+
+export default meanSimple;
