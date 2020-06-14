a89895a634db612d1346ab2b9788338655c3f1e6
Tom MacWright <tom@macwright.org>
2020-06-13 17:46:17 -0700

    chore: Make standardError throw for 1-length lists. Add tests

--- test/standard-error.test.js
+++ test/standard-error.test.js
@@ -1,12 +1,29 @@
 /* eslint no-shadow: 0 */
 
+const test = require("tap").test;
+const ss = require("../");
 
-var test = require('tap').test;
-var ss = require('../');
+test("standardError", function (t) {
+    t.test(
+        "can get the standard error of the wikipedia standard deviation example",
+        function (t) {
+            t.equal(
+                ss.standardError([2, 4, 4, 4, 5, 5, 7, 9]),
+                2 / Math.sqrt(8)
+            );
+            t.end();
+        }
+    );
 
-test('standardError', function(t) {
-    t.test('can get the standard error of the wikipedia standard deviation example', function(t) {
-        t.equal(ss.standardError([2, 4, 4, 4, 5, 5, 7, 9]), 2 / Math.sqrt(8));
+    t.test("is zero for single-element arrays", function (t) {
+        t.equal(ss.standardError([42]), 0);
+        t.end();
+    });
+
+    t.test("is not defined for empty arrays", function (t) {
+        t.throws(function () {
+            ss.standardError([]);
+        }, "standardError is not defined for empty arrays");
         t.end();
     });
 
