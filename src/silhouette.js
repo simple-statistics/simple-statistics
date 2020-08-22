d651dadc0b3f1b1be174f158c39fb1b345b71192
Greg Wilson <gvwilson@third-bit.com>
2020-08-22 18:04:01 -0400

    Incorporating changes from feedback.
    -   Adding example to documentation.
    -   Using arr.length instead of caching lengths.
    -   Using epsilon instead of defining own tolerance.

--- src/silhouette.js
+++ src/silhouette.js
@@ -1,23 +1,27 @@
 import euclideanDistance from "./euclidean_distance";
 import makeMatrix from "./make_matrix";
+import max from "./max";
 
 /**
  * Calculate the [silhouette values](https://en.wikipedia.org/wiki/Silhouette_(clustering))
  * for clustered data.
+ *
  * @param {Array<Array<number>>} points N-dimensional coordinates of points.
  * @param {Array<number>} labels Labels of points. This must be the same length as `points`,
  * and values must lie in [0..G-1], where G is the number of groups.
- * @returns {Array<number>} The silhouette value for each point.
+ * @return {Array<number>} The silhouette value for each point.
+ *
+ * @example
+ * silhouette([[0.25], [0.75]], [0, 0]); // => [1.0, 1.0]
  */
 function silhouette(points, labels) {
     if (points.length !== labels.length) {
         throw new Error("must have exactly as many labels as points");
     }
-    const numPoints = points.length,
-          groupings = createGroups(labels),
-          distances = calculateAllDistances(points);
+    const groupings = createGroups(labels);
+    const distances = calculateAllDistances(points);
     const result = [];
-    for (let i = 0; i < numPoints; i++){
+    for (let i = 0; i < points.length; i++) {
         let s = 0;
         if (groupings[labels[i]].length > 1) {
             const a = meanDistanceWithinGroup(i, labels, groupings, distances);
@@ -31,18 +35,19 @@ function silhouette(points, labels) {
 
 /**
  * Create a lookup table mapping group IDs to point IDs.
+ *
+ * @private
  * @param {Array<number>} labels Labels of points. This must be the same length as `points`,
  * and values must lie in [0..G-1], where G is the number of groups.
- * @returns {Array<Array<number>>} An array of length G, each of whose entries is an array
+ * @return {Array<Array<number>>} An array of length G, each of whose entries is an array
  * containing the indices of the points in that group.
  */
 function createGroups(labels) {
-    const numLabels = labels.length,
-          numGroups = 1 + Math.max(...labels),
-          result = Array(numGroups);
-    for (let i = 0; i < numLabels; i++){
+    const numGroups = 1 + max(labels);
+    const result = Array(numGroups);
+    for (let i = 0; i < labels.length; i++) {
         const label = labels[i];
-        if (result[label] === undefined){
+        if (result[label] === undefined) {
             result[label] = [];
         }
         result[label].push(i);
@@ -52,15 +57,17 @@ function createGroups(labels) {
 
 /**
  * Create a lookup table of all inter-point distances.
+ *
+ * @private
  * @param {Array<Array<number>>} points N-dimensional coordinates of points.
- * @returns {Array<Array<number>>} A symmetric square array of inter-point distances
+ * @return {Array<Array<number>>} A symmetric square array of inter-point distances
  * (zero on the diagonal).
  */
 function calculateAllDistances(points) {
-    const numPoints = points.length,
-          result = makeMatrix(numPoints, numPoints);
-    for (let i = 0; i < numPoints; i++){
-        for (let j = 0; j < i; j++){
+    const numPoints = points.length;
+    const result = makeMatrix(numPoints, numPoints);
+    for (let i = 0; i < numPoints; i++) {
+        for (let j = 0; j < i; j++) {
             result[i][j] = euclideanDistance(points[i], points[j]);
             result[j][i] = result[i][j];
         }
@@ -71,13 +78,15 @@ function calculateAllDistances(points) {
 /**
  * Calculate the mean distance between a point and other points in its group
  * (which is zero if the group only has one element).
+ *
+ * @private
  * @param {number} which The index of this point.
  * @param {Array<number>} labels Labels of points.
  * @param {Array<Array<number>>} groupings An array whose entries are arrays
  * containing the indices of the points in that group.
  * @param {Array<Array<number>>} distances A symmetric square array of inter-point
  * distances.
- * @returns {number} The mean distance from this point to others in its group.
+ * @return {number} The mean distance from this point to others in its group.
  */
 function meanDistanceWithinGroup(which, labels, groupings, distances) {
     return meanDistanceFromPointToGroup(which, groupings[labels[which]], distances);
@@ -86,23 +95,24 @@ function meanDistanceWithinGroup(which, labels, groupings, distances) {
 /**
  * Calculate the mean distance between this point and all the points in the
  * nearest group (as determined by which point in another group is closest).
+ *
+ * @private
  * @param {number} which The index of this point.
  * @param {Array<number>} labels Labels of points.
  * @param {Array<Array<number>>} groupings An array whose entries are arrays
  * containing the indices of the points in that group.
  * @param {Array<Array<number>>} distances A symmetric square array of inter-point
  * distances.
- * @returns {number} The mean distance from this point to others in the nearest
+ * @return {number} The mean distance from this point to others in the nearest
  * group.
  */
 function meanDistanceToNearestGroup(which, labels, groupings, distances) {
-    const label = labels[which],
-          numGroups = groupings.length;
+    const label = labels[which];
     let result = Number.MAX_VALUE;
-    for (let i = 0; i < numGroups; i++){
-        if (i !== label){
+    for (let i = 0; i < groupings.length; i++) {
+        if (i !== label) {
             const d = meanDistanceFromPointToGroup(which, groupings[i], distances);
-            if (d < result){
+            if (d < result) {
                 result = d;
             }
         }
@@ -113,21 +123,22 @@ function meanDistanceToNearestGroup(which, labels, groupings, distances) {
 /**
  * Calculate the mean distance between a point and all the points in a group
  * (possibly its own).
+ *
+ * @private
  * @param {number} which The index of this point.
  * @param {Array<number>} group The indices of all the points in the group in
  * question.
  * @param {Array<Array<number>>} distances A symmetric square array of inter-point
  * distances.
- * @returns {number} The mean distance from this point to others in the
+ * @return {number} The mean distance from this point to others in the
  * specified group.
  */
 function meanDistanceFromPointToGroup(which, group, distances) {
-    const groupSize = group.length;
-    let total = 0.0;
-    for (let i = 0; i < groupSize; i++){
+    let total = 0;
+    for (let i = 0; i < group.length; i++) {
         total += distances[which][group[i]];
     }
-    return total / groupSize;
+    return total / group.length;
 }
 
 export default silhouette;
