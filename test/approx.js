1bb6a353036a4816f4e246e91fd83d6c39e997ee
Greg Wilson <gvwilson@third-bit.com>
2020-08-22 18:04:01 -0400

    Incorporating changes from feedback.
    -   Adding example to documentation.
    -   Using arr.length instead of caching lengths.
    -   Using epsilon instead of defining own tolerance.

--- test/approx.js
+++ test/approx.js
@@ -1,43 +1,40 @@
-/**
- * Default tolerance for relative error.
- */
-const TOLERANCE = 1.0e-6;
+const ss = require("../");
 
 /**
  * Relative error.
+ *
  * @param {number} a One value.
  * @param {number} a The other value.
- * @returns {number} The relative error.
+ * @return {number} The relative error.
  */
 function relativeError(a, b) {
     if (a === b) {
-        return 0.0;
+        return 0;
     }
     return Math.abs(a - b) / Math.max(Math.abs(a), Math.abs(b));
 }
 
 /**
  * Approximate equality.
+ *
  * @param {number} a One value.
  * @param {number} a The other value.
  * @param {number} tolerance The acceptable relative difference.
- * @returns {boolean} Whether numbers are within tolerance.
+ * @return {boolean} Whether numbers are within tolerance.
  */
-function approxEqual(a, b, tolerance = TOLERANCE) {
-    if (tolerance < 0.0) {
-        throw new Error("tolerance must be non-negative");
-    }
+function approxEqual(a, b, tolerance = ss.epsilon) {
     return relativeError(a, b) <= tolerance;
 }
 
 /**
  * Approximate equality for all elements of two vectors.
+ *
  * @param {number} a One vector.
  * @param {number} a The other vector.
  * @param {number} tolerance The acceptable relative difference between elements.
- * @returns {boolean} Whether all numbers are within tolerance.
+ * @return {boolean} Whether all numbers are within tolerance.
  */
-function allApproxEqual(a, b, tolerance = TOLERANCE) {
+function allApproxEqual(a, b, tolerance = ss.epsilon) {
     if (a.length !== b.length) {
         throw new Error("input vectors must have the same length")
     }
@@ -51,7 +48,6 @@ function allApproxEqual(a, b, tolerance = TOLERANCE) {
 }
 
 export {
-    TOLERANCE,
     relativeError,
     approxEqual,
     allApproxEqual
