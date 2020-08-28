be410b06aa5e720f1afabae2569287aa5de5d8cf
Greg Wilson <gvwilson@third-bit.com>
2020-08-28 06:23:55 -0400

    Relative error and approximate equality.
    1.  Move relative error and approximate equality out of `test/approx.js` helper file into `src/relative_error.js`, `src/approx_strict_equal.js`, and `src/deep_approx_strict_equal.js`.
    1.  Add tests.
    1.  Add documentation and TypeScript `.d.ts` files.
    1.  Add `testonly` target to `package.json` to run tests single-threaded without linting or coverage for quicker turnaround during dev.

---
+++ src/deep_approx_strict_equal.d.ts
@@ -0,0 +1,10 @@
+/**
+ * https://simplestatistics.org/docs/#deep_approx_strict_equal
+ */
+declare function deepApproxStrictEqual(
+    actual: any,
+    expected: any,
+    tolerance: number
+) : boolean
+
+export default deepApproxStrictEqual;
