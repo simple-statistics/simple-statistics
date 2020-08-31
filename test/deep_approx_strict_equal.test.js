7f8b1ad47e5d6c8a51add74ec3a946d7365f1c49
Greg Wilson <gvwilson@third-bit.com>
2020-08-31 16:14:21 -0400

    Removing deep object equality

--- test/deep_approx_strict_equal.test.js
+++ test/deep_approx_strict_equal.test.js
@@ -13,8 +13,7 @@ test("deep approximate equality", function (t) {
             ["null", null],
             ["boolean", false],
             ["string", "abc"],
-            ["number", 3],
-            ["object", { key: "value" }]
+            ["number", 3]
         ];
         for (const [leftType, leftVal] of fixtures) {
             for (const [rightType, rightVal] of fixtures) {
@@ -26,9 +25,6 @@ test("deep approximate equality", function (t) {
                 }
             }
         }
-
-        t.true(!ss.deepApproxStrictEqual([1], { a: 1 }));
-        t.true(!ss.deepApproxStrictEqual({ a: 1 }, [1]));
         t.end();
     });
 
@@ -47,29 +43,8 @@ test("deep approximate equality", function (t) {
         t.end();
     });
 
-    t.test("does not think scalars and objects are equal", function (t) {
-        t.true(!ss.deepApproxStrictEqual(10, { ten: 10 }));
-        t.end();
-    });
-
     t.test("handles undefined values", function (t) {
         t.true(ss.deepApproxStrictEqual(undefined, undefined));
-        t.end();
-    });
-
-    t.test("handles functions", function (t) {
-        const left = function () {
-            return 0;
-        };
-        const alsoLeft = function () {
-            return 0;
-        };
-        const right = function () {
-            return 1;
-        };
-        t.true(ss.deepApproxStrictEqual(left, left));
-        t.true(!ss.deepApproxStrictEqual(left, alsoLeft));
-        t.true(!ss.deepApproxStrictEqual(left, right));
         t.end();
     });
 
@@ -84,54 +59,6 @@ test("deep approximate equality", function (t) {
         t.true(ss.deepApproxStrictEqual([1 + HALF_EPS, -1 + HALF_EPS],
                                         [1, -1]));
         t.true(!ss.deepApproxStrictEqual([1, 1], [1, 1 + TWO_EPS]));
-        t.end();
-    });
-
-    t.test("objects equal within tolerance", function (t) {
-        t.true(ss.deepApproxStrictEqual({ a: 1 }, { a: 1 + HALF_EPS }));
-        t.end();
-    });
-
-    t.test("objects not equal outside tolerance", function (t) {
-        t.true(!ss.deepApproxStrictEqual({ a: 1 }, { a: 1 + TWO_EPS }));
-        t.end();
-    });
-
-    t.test("objects not equal with extra keys", function (t) {
-        t.true(!ss.deepApproxStrictEqual({ a: 1, b: 1 }, { a: 1 }));
-        t.end();
-    });
-
-    t.test("compares arrays of objects", function (t) {
-        t.true(ss.deepApproxStrictEqual([{ a: 1 - HALF_EPS },
-                                         { b: 1 + HALF_EPS }],
-                                        [{ a: 1 },
-                                         { b: 1 }]));
-        t.true(!ss.deepApproxStrictEqual([{ a: 1 - HALF_EPS },
-                                          { b: 1 + HALF_EPS }],
-                                         [{ a: 1},
-                                          { b: 1 + TWO_EPS }]));
-        t.end();
-    });
-
-    t.test("compares objects with arrays", function (t) {
-        t.true(ss.deepApproxStrictEqual({ a: [1, 2],
-                                          b: [1 + HALF_EPS, 2 - HALF_EPS] },
-                                        { a: [1, 2],
-                                          b: [1, 2] }));
-        t.true(!ss.deepApproxStrictEqual({ a: [1, 2],
-                                           b: [1 + TWO_EPS, 2 - HALF_EPS] },
-                                         { a: [1, 2],
-                                           b: [1, 2 - HALF_EPS] }));
-        t.end();
-    });
-
-    t.test("compares nested structures with different tolerance", function (t) {
-        t.true(!ss.deepApproxStrictEqual({ a: [1, 2], b: [1 + TWO_EPS, 2] },
-                                         { a: [1, 2], b: [1, 2] }));
-        t.true(ss.deepApproxStrictEqual({ a: [1, 2], b: [1, 2] },
-                                        { a: [1, 2], b: [1, 2] },
-                                        4 * EPS));
         t.end();
     });
 
