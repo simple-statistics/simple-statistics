f289fd11bd4eafcd5df319979b051a030bf60f88
Tom MacWright <tmcw@users.noreply.github.com>
2020-09-07 21:39:20 -0700

    feat: Add randomSource argument to permutationTest (#505)

--- test/permutation_test.test.js
+++ test/permutation_test.test.js
@@ -1,13 +1,24 @@
 /* eslint no-shadow: 0 */
 
 const test = require("tap").test;
+const Random = require("random-js");
+const random = new Random.Random(Random.MersenneTwister19937.seed(0));
 const ss = require("../");
+
+function rng() {
+    return random.real(0, 1);
+}
 
 test("permutation test", function (t) {
     t.test(
         "P-value of identical distributions being different should be 1",
         function (t) {
-            t.equal(ss.permutationTest([2, 2, 2, 2, 2], [2, 2, 2, 2, 2]), 1);
+            t.equal(
+                ss.permutationTest([2, 2, 2, 2, 2], [2, 2, 2, 2, 2]),
+                1,
+                undefined,
+                rng
+            );
             t.end();
         }
     );
@@ -15,7 +26,13 @@ test("permutation test", function (t) {
         t
     ) {
         t.equal(
-            ss.permutationTest([2, 2, 2, 2, 2], [2, 2, 2, 2, 2], "greater"),
+            ss.permutationTest(
+                [2, 2, 2, 2, 2],
+                [2, 2, 2, 2, 2],
+                "greater",
+                undefined,
+                rng
+            ),
             1
         );
         t.end();
@@ -38,7 +55,9 @@ test("permutation test", function (t) {
                         99999,
                         99999
                     ],
-                    "less"
+                    "less",
+                    undefined,
+                    rng
                 ) < ss.epsilon
             );
             t.end();
