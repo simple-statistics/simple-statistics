6fecf20eb7f9747b733b30ac5babbae6b6048f4c
Daniel Saxton <dsaxton@pm.me>
2021-03-03 17:04:35 -0600

    Update normal CDF doc

--- src/standard_normal_table.js
+++ src/standard_normal_table.js
@@ -18,15 +18,10 @@ function cumulativeDistribution(z) {
 /**
  * A standard normal table, also called the unit normal table or Z table,
  * is a mathematical table for the values of Φ (phi), which are the values of
- * the cumulative distribution function of the normal distribution.
- * It is used to find the probability that a statistic is observed below,
- * above, or between values on the standard normal distribution, and by
- * extension, any normal distribution.
- *
- * The probabilities are calculated using the
- * [Cumulative distribution function](https://en.wikipedia.org/wiki/Normal_distribution#Cumulative_distribution_function).
- * The table used is the cumulative, and not cumulative from 0 to mean
- * (even though the latter has 5 digits precision, instead of 4).
+ * the [cumulative distribution function](https://en.wikipedia.org/wiki/Normal_distribution#Cumulative_distribution_function)
+ * of the normal distribution. It is used to find the probability that a
+ * statistic is observed below, above, or between values on the standard
+ * normal distribution, and by extension, any normal distribution.
  */
 const standardNormalTable = [];
 
