66bb515d143e94f55060c576dcbd95f6c63fba24
Tom MacWright <tom@macwright.com>
2025-08-25 10:06:37 -0400

    Re-run biome

--- src/jenks.d.ts
+++ src/jenks.d.ts
@@ -1,9 +1,6 @@
 /**
  * https://simple-statistics.github.io/docs/#jenks
  */
-declare function jenks(
-    data: readonly number[],
-    nClasses: number
-): number[];
+declare function jenks(data: readonly number[], nClasses: number): number[];
 
 export default jenks;
