d3007eef33f4942e7fbe10e8f7a65927c5c7c691
Tom MacWright <tom@macwright.com>
2025-08-27 11:33:17 -0400

    Extend to ckmeans

--- src/ckmeans.d.ts
+++ src/ckmeans.d.ts
@@ -1,6 +1,6 @@
 /**
  * https://simple-statistics.github.io/docs/#ckmeans
  */
-declare function ckmeans(x: number[], nClusters: number): number[][];
+declare function ckmeans(x: readonly number[], nClusters: number): number[][];
 
 export default ckmeans;
