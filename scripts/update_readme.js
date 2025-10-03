09ede3fdd8b736ba1eca3cb4675279ae58bd9589
Tom MacWright <tom@macwright.com>
2025-10-03 10:19:52 -0400

    Update biome config, use vcs, run check write

--- scripts/update_readme.js
+++ scripts/update_readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
-var fs = require('fs');
+var fs = require("node:fs");
 
-var version = JSON.parse(fs.readFileSync('./package.json')).version;
-var README = fs.readFileSync('./README.md', 'utf8');
+var version = JSON.parse(fs.readFileSync("./package.json")).version;
+var README = fs.readFileSync("./README.md", "utf8");
 fs.writeFileSync(
-    'README.md',
-    README.replace(/simple-statistics@([\d\.]+)/g,
-    'simple-statistics@' + version));
+    "README.md",
+    README.replace(
+        /simple-statistics@([\d.]+)/g,
+        "simple-statistics@" + version
+    )
+);
